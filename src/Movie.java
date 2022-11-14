c32048f0882b7a3d2a7454892630e83ec05e73c1
eyita <me@ME>
2022-11-14 18:21:59 -0500

    test2

--- src/Movie.java
+++ src/Movie.java
@@ -4,47 +4,6 @@ import java.sql.Array;
 import java.text.DecimalFormat;
 import java.util.*;
 
-//public class Sorted {
-//    static void sort(List<Movie> array, int lowindex, int highIndex){
-//        if(lowindex>=highIndex){
-//            return;
-//        }
-//
-//        Movie pivot = array.get(highIndex);
-//         int leftp = lowindex;
-//        int rightp =highIndex;
-//
-//        //sort the array each run
-//        while(leftp<rightp){
-//            while (array.get[leftp]<= pivot && leftp<rightp){
-//                leftp++;
-//            }
-//            while (array.get[rightp]>= pivot && leftp<rightp){
-//                rightp--;
-//            }
-//
-//            swap(array, leftp,rightp);
-//
-//        }
-//
-//        //swap the pivot with the meet index
-//        swap(array, leftp, highIndex);
-//
-//        //sort left side
-//        sort(array,lowindex,leftp-1);
-//
-//        //sort right side
-//        sort(array,leftp,highIndex);
-//    }
-//
-//    private static void swap(List<Movie> array,int index1, int index2){
-//        Movie temp = array.get(index1);
-//        Movie temp2 = array.get(index2);
-//        array.set(index1,temp2);
-//        array.set(index2,temp);
-//
-//
-//    }
 
 
 
